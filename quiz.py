e13c41b92a0bf3a2bcbcf001bb8af72782188919
Kaio Fernandes <kaion_dev@hotmail.com>
2025-12-16 16:33:23 -0300

    concluido quiz melhorei o código

--- quiz.py
+++ quiz.py
@@ -1,24 +1,31 @@
+import time
 #Lista com dicionários para organização de questão, alternativas e resposta.
 questions = [
     {
         'Pergunta': 'Quanto é 2+2?',
         'Opções': ['1', '3', '4', '5'],
-        'Resposta': '4',
+        'Resposta': 'C',
     },
 
     {
         'Pergunta': 'Quanto é 5*5?',
         'Opções': ['25', '55', '10', '51'],
-        'Resposta': '25',
+        'Resposta': 'A',
     },
 
     {
         'Pergunta': 'Quanto é 10/2',
         'Opções': ['4', '5', '2', '1'],
-        'Resposta': '5',
+        'Resposta': 'B',
     },
+
+    {
+        'Pergunta': 'Quanto é 100+22',
+        'Opções': ['122', '123', '240', '121'],
+        'Resposta': 'A',
+    }
 ]
-#Variavel para aparecer letras as questões // Contagem de perguntas corretas
+#Variavel para mostrar letras as questões // Contagem de perguntas corretas
 words = ['A', 'B', 'C', 'D']
 question_right = 0
 
@@ -31,5 +38,16 @@ for question_for_user in questions:
         print(f'({words[i]}) {option_for_user}')
         
     #Bloco de resposta do usuario para verificar se a opção escolhida está certa ou incorreta
-    response_user = input('Digite a alternativa: ').upper()
-    if response_user == questions[0]['Resposta']:
+    response_user = input('Digite a sua alternativa: ').upper()
+    if response_user == question_for_user['Resposta']:
+        print('Você acertou!')
+        question_right += 1
+        time.sleep(2)
+    else:
+        print('Você errou!')
+        time.sleep(2)
+
+print(f'Parabéns, seu resultado: 3/{question_right}')
+    
+
+    
